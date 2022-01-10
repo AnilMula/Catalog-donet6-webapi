4ec19252210869d1db804599c68270f2d12b6794
Anil Kumar Mula <AKM@it-kartellet.dk>
2022-01-10 11:39:30 +0100

    Entities Repository and Controllers are added

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Catalog.Repositories;
+using System.Collections.Generic;
+using Catalog.Entities;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]  // GET /items
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")] // GET /items/{id}
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+    }
+}
