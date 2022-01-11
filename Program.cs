15734ae9976a556a1f9d2826cb0798923cc93c7a
Anil Kumar Mula <AKM@it-kartellet.dk>
2022-01-11 08:26:29 +0100

    adding support for connecting to mongoDB

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
 using Catalog.Repositories;
+using MongoDB.Driver;
+using Catalog.Settings;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//  var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+// return new MongoClient(mongoDbSettings.ConnectionString);
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>(); // register dependency
+builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
+{
+    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    return new MongoClient(mongoDbSettings.ConnectionString);
+});
+
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>(); // register dependency
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
